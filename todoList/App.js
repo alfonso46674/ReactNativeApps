e4dfdfd4d5ecb0a3b58e975b879d1581b46a2fb4
alfonso.ramirez <is713932@iteso.mx>
2021-07-12 21:18:38 -0500

    Feat: Add bar to create new task

--- todoList/App.js
+++ todoList/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task/Task'
 
 
@@ -16,6 +16,18 @@ export default function App() {
         </View>
 
       </View>
+
+    {/* Add tasks */}
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding':'height'}
+    style={styles.writeTaskWrapper}>
+      <TextInput style={styles.input} placeholder={'Write a task'}></TextInput>
+    
+      <TouchableOpacity>
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
 
     </View>
   );
@@ -36,5 +48,36 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop: 30
+  },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    alignItems: 'center'
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    width: 250,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+
+  },
+  addWrapper:{
+    width: 60,
+    height:60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#C0C0C0'
+  },
+  addText:{
+
   }
 });
